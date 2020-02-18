301716c276892b15e6d7a185a86e4ba5ee8f9e71
LuciusGen <sergey.102333@gmail.com>
2020-02-18 18:40:35 +0300

    #1, Добавлено преобразование задачи в строку(для вывода)

--- venv/examp.py
+++ venv/examp.py
@@ -4,6 +4,3 @@ C = [1, 2, 3, 4]
 A = []
 
 
-
-class Examp(task.Task):
-    def __init__(self):
--- venv/task.py
+++ venv/task.py
@@ -1,5 +1,30 @@
+#мы подразумеваем, что у нас каноническая задача
+import numpy as np
+
+
 class Task:
     def __init__(self, C, X, A, B):
         self.C = C
         self.A = A
         self.B = B
+
+    def __str__(self):
+        Matr = list()
+        for i in range(len(self.A)):
+            for j in range(len(self.A[i])):
+                Matr.append(str(self.A[j]) + ' ');
+
+            Matr.append('=' + str(self.B[i]) + '\n')
+
+        for i in range(len(self.C)):
+            Matr.append(str(self.C) + 'X' + str(i + 1))
+
+        Matr.append('-> min')
+
+        return Matr
+
+    def getReverseA(self):
+        return np.linalg.inv(A)
+
+    def getRand(self):
+        return np.linalg.matrix_rank(A)
